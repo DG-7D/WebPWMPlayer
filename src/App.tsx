cba2d9947646a6c931e12fa14739bed4258eeb30
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-02-21 11:25:41 +0900

    可読性を改善

--- src/App.tsx
+++ src/App.tsx
@@ -8,36 +8,27 @@ function PwmGenerator() {
     const audioContextRef = React.useRef<AudioContext>(undefined);
     const sourceRef = React.useRef<AudioBufferSourceNode>(undefined);
 
-    const [lambda, setLambda] = React.useState<number>(20000);
-    const [width, setWidth] = React.useState<number>(1500);
+    const [waveMs, setWaveMs] = React.useState<number>(20000);
+    const [pulseMs, setPulseMs] = React.useState<number>(1500);
     const [inverted, setInverted] = React.useState<boolean>(false);
     const [playing, setPlaying] = React.useState<boolean>(false);
     const [cutDc, setCutDc] = React.useState<boolean>(true);
 
     function handleLambdaChange(event: React.ChangeEvent<HTMLInputElement>) {
         const newLambda = event.target.valueAsNumber;
-        setLambda(newLambda);
-        setWidth(Math.min(width, newLambda));
+        setWaveMs(newLambda);
+        setPulseMs(Math.min(pulseMs, newLambda));
     }
     function handleFreqChange(event: React.ChangeEvent<HTMLInputElement>) {
         const newLambda = 1e6 / event.target.valueAsNumber;
-        setLambda(newLambda);
-        setWidth(width * newLambda / lambda);
+        setWaveMs(newLambda);
+        setPulseMs(pulseMs * newLambda / waveMs);
     }
     function handlePulseChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setWidth(event.target.valueAsNumber);
+        setPulseMs(event.target.valueAsNumber);
     }
     function handleDutyChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setWidth(lambda * event.target.valueAsNumber / 100);
-    }
-    function handleInvertChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setInverted(event.target.checked);
-    }
-    function handlePlayClick() {
-        setPlaying(true);
-    }
-    function handleStopClick() {
-        setPlaying(false);
+        setPulseMs(waveMs * event.target.valueAsNumber / 100);
     }
 
     React.useEffect(() => {
@@ -62,57 +53,57 @@ function PwmGenerator() {
         sourceRef.current?.disconnect();
 
         const sampleRate = audioContextRef.current.sampleRate;
-        const loopLength = Math.floor(sampleRate * lambda / 1e6);
-        if (loopLength < 1) {
+        const waveSamples = Math.floor(sampleRate * waveMs / 1e6);
+        if (waveSamples < 1) {
             return;
         }
-        const pulseLength = Math.floor(sampleRate * width / 1e6);
+        const pulseSamples = Math.floor(sampleRate * pulseMs / 1e6);
 
         let highLevel = 1;
         let lowLevel = -1;
         if (cutDc) {
-            if (pulseLength < loopLength / 2) {
+            if (pulseSamples < waveSamples / 2) {
                 highLevel = 1;
-                lowLevel = -pulseLength / (loopLength - pulseLength);
+                lowLevel = -pulseSamples / (waveSamples - pulseSamples);
             } else {
-                highLevel = (loopLength - pulseLength) / pulseLength;
+                highLevel = (waveSamples - pulseSamples) / pulseSamples;
                 lowLevel = -1;
             }
         }
         const buffer = new AudioBuffer({
-            length: loopLength,
+            length: waveSamples,
             numberOfChannels: inverted ? 2 : 1,
             sampleRate: sampleRate,
         });
-        buffer.getChannelData(0).fill(highLevel).fill(lowLevel, pulseLength);
-        inverted && buffer.getChannelData(1).fill(lowLevel).fill(highLevel, pulseLength);
+        buffer.getChannelData(0).fill(highLevel).fill(lowLevel, pulseSamples);
+        inverted && buffer.getChannelData(1).fill(lowLevel).fill(highLevel, pulseSamples);
         sourceRef.current = new AudioBufferSourceNode(audioContextRef.current, { buffer: buffer, loop: true });
 
         sourceRef.current.connect(audioContextRef.current.destination);
         sourceRef.current.start();
-    }, [lambda, width, inverted, playing, cutDc]);
+    }, [waveMs, pulseMs, inverted, playing, cutDc]);
 
     return (
         <>
             <h1>パルス幅変調再生器</h1>
             <div>
-                <label>波長</label> <input type="number" value={Math.round(lambda)} min={1e6 / freqMax} max={1e6 / freqMin} step={100} onChange={handleLambdaChange} /> μs,
-                <label>周波数</label> <input type="number" value={Math.round(1e6 / lambda)} min={freqMin} max={freqMax} step={1} onChange={handleFreqChange} /> Hz
+                <label>波長</label> <input type="number" value={Math.round(waveMs)} min={1e6 / freqMax} max={1e6 / freqMin} step={100} onChange={handleLambdaChange} /> μs,
+                <label>周波数</label> <input type="number" value={Math.round(1e6 / waveMs)} min={freqMin} max={freqMax} step={1} onChange={handleFreqChange} /> Hz
             </div>
             <div>
-                <label>パルス幅</label> <input type="number" value={Math.round(width)} min={0} max={Math.ceil(lambda)} step={100} onChange={handlePulseChange} /> μs,
-                <label>デューティ比</label> <input type="number" value={Math.round(width / lambda * 100 * 10) / 10} min={0} max={100} step={0.5} onChange={handleDutyChange} /> %
+                <label>パルス幅</label> <input type="number" value={Math.round(pulseMs)} min={0} max={Math.ceil(waveMs)} step={100} onChange={handlePulseChange} /> μs,
+                <label>デューティ比</label> <input type="number" value={Math.round(pulseMs / waveMs * 100 * 10) / 10} min={0} max={100} step={0.5} onChange={handleDutyChange} /> %
             </div>
             <div>
-                <label>右チャンネルを反転</label> <input type="checkbox" checked={inverted} onChange={handleInvertChange} />
+                <label>右チャンネルを反転</label> <input type="checkbox" checked={inverted} onChange={(event) => setInverted(event.target.checked)} />
             </div>
             <div>
                 <label>直流成分をカット</label> <input type="checkbox" checked={cutDc} onChange={(event) => setCutDc(event.target.checked)} />
             </div>
             <div>
                 {playing
-                    ? <button onClick={handleStopClick}>停止</button>
-                    : <button onClick={handlePlayClick}>再生</button>}
+                    ? <button onClick={() => setPlaying(false)}>停止</button>
+                    : <button onClick={() => setPlaying(true)}>再生</button>}
             </div>
         </>
     );
